9ed77027fd0e1ec6b22cd0550ef4ecf3a74a27de
Andrew <andrew.dale.wylie@gmail.com>
2012-07-25 19:13:15 -0400

    Added a test for counter-clockwise rotation.

--- src/tests/TileTest.java
+++ src/tests/TileTest.java
@@ -44,4 +44,42 @@ public class TileTest {
 
         assertTrue(theSame);
     }
+
+    @Test
+    public void rotateCounterClockwiseTest() {
+
+        char[][] testTileArr =
+            new char[][] { { 'x', 'C', 'C', 'C', 'C', 'C', 'x' },
+                    { 'F', 'F', 'C', 'C', 'C', 'C', 'C' },
+                    { 'F', 'F', 'F', 'F', 'C', 'C', 'C' },
+                    { 'R', 'R', 'R', 'F', 'F', 'C', 'C' },
+                    { 'F', 'F', 'R', 'R', 'F', 'C', 'C' },
+                    { 'F', 'F', 'F', 'R', 'F', 'F', 'C' },
+                    { 'x', 'F', 'F', 'R', 'F', 'F', 'x' } };
+
+        char[][] oracleTileArr =
+            new char[][] { { 'x', 'C', 'C', 'C', 'C', 'C', 'x' },
+                    { 'C', 'C', 'C', 'C', 'C', 'F', 'F' },
+                    { 'C', 'C', 'C', 'F', 'F', 'F', 'F' },
+                    { 'C', 'C', 'F', 'F', 'R', 'R', 'R' },
+                    { 'C', 'C', 'F', 'R', 'R', 'F', 'F' },
+                    { 'C', 'F', 'F', 'R', 'F', 'F', 'F' },
+                    { 'x', 'F', 'F', 'R', 'F', 'F', 'x' } };
+
+        Tile testTile = new Tile(testTileArr, "CHR2");
+        Tile oracleTile = new Tile(oracleTileArr, "CHR2");
+        testTile.rotateCounterClockwise();
+
+        boolean theSame = true;
+
+        for (int i = 0; i < Tile.tileSize; i++) {
+            for (int j = 0; j < Tile.tileSize; j++) {
+                if (testTile.getTileType(i, j) != oracleTile.getTileType(i, j)) {
+                    theSame = false;
+                }
+            }
+        }
+
+        assertTrue(theSame);
+    }
 }
