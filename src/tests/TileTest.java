f08ff28bea04f6fe0d7781f571dd14caedd681d2
Andrew <andrew.dale.wylie@gmail.com>
2012-07-25 17:54:45 -0400

    Added testing to appease CI gods. More tests to come though, I promise!

---
+++ src/tests/TileTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TileTest {
+
+    @Test
+    public void fakeTest() {
+        assertTrue(true);
+    }
+}
